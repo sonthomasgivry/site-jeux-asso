5b476a6b5debc435e348fbe7c119471bebb814a0
sonthomasgivry <sonthomasgivry@gmail.com>
2026-08-21 10:30:09 +0200

    Add files via upload

--- api/getJeux.js
+++ api/getJeux.js
@@ -1,25 +1,33 @@
 // Fichier : api/getJeux.js
 export default async function handler(req, res) {
-    // Vercel gardera ces infos secrètes pour nous
-    const NOTION_SECRET = process.env.NOTION_SECRET;
-    const DATABASE_ID = process.env.DATABASE_ID;
+    if (req.method !== 'GET') return res.status(405).end();
+
+    const { NOTION_SECRET, DATABASE_ID } = process.env;
 
     try {
-        // On tape à la porte de Notion
-        const response = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
+        const notionRes = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${NOTION_SECRET}`,
                 'Notion-Version': '2022-06-28',
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify({
+                // LE TRI : Du plus grand nombre de votes au plus petit
+                sorts: [
+                    {
+                        property: 'Votes',
+                        direction: 'descending'
+                    }
+                ]
+            })
         });
 
-        const data = await response.json();
-        
-        // On renvoie les données à ton site web
-        res.status(200).json(data.results);
+        if (!notionRes.ok) throw new Error("Erreur Notion getJeux");
+
+        const data = await notionRes.json();
+        return res.status(200).json(data.results);
     } catch (error) {
-        res.status(500).json({ error: 'Erreur lors de la connexion à Notion' });
+        return res.status(500).json({ error: "Erreur serveur getJeux" });
     }
 }
